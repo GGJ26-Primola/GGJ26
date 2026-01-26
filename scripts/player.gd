b45e1287ed0cf43ae9aad1b5faac5f1d5138b689
MaxRomagnoli <info@maxromagnoli.com>
2026-01-26 17:38:33 +0100

    added phantom camera addon

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+@onready var animated_sprite: AnimatedSprite3D = $AnimatedSprite3D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,18 +12,22 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		animated_sprite.play("jump_front")
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+	
+	elif is_on_floor():
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var input_dir := Input.get_vector("left", "right", "up", "down")
+		var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		if direction:
+			animated_sprite.play("walk_front")
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
+		else:
+			animated_sprite.play("idle")
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
