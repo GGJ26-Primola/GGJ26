fc78dc642e5ab73b081fb8abeff813f7042b0de4
MaxRomagnoli <info@maxromagnoli.com>
2026-01-27 10:50:14 +0100

    test dialogic

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -4,14 +4,11 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Dialogic.start("timeline")
 	#Dialogic.VAR.response = "test"
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("ui_accept"):
-		camera_player.follow_offset = Vector3(0, 2, 2)
-	else:
-		camera_player.follow_offset = Vector3(0, 5, 5)
+	
+	if Input.is_action_pressed("lanterna"):
+		Dialogic.start("timeline")
