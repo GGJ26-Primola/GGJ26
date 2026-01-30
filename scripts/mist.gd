2655a7c1e34fbb23bac1b07c35fabeb0dc919d67
vinceG28 <vincenzo.grimaudo@studio.unibo.it>
2026-01-30 17:59:03 +0100

    added boss animations

--- scripts/mist.gd
+++ scripts/mist.gd
@@ -3,9 +3,17 @@ extends FogVolume
 @onready var collision: CollisionShape3D = $Hitbox/CollisionShape3D
 var disabled: bool
 
+func _ready() -> void:
+	disabled = true
+
 func _process(delta: float) -> void:
 	collision.disabled = disabled
 
 func _on_hitbox_body_entered(body: Node3D) -> void:
 	if body.name == "Player" and Dialogic.VAR.current_mask != "pest":
 		Global.game_over = true
+
+
+func _on_hitbox_body_exited(body: Node3D) -> void:
+	if body.name == "Player" and Dialogic.VAR.current_mask != "pest":
+		Global.game_over = false
